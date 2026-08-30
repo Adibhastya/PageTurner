34a99b05230763bd81b89fb8550cc251de0f6ccd
Aditya Bhaskar <bhaskaraditya65@gmail.com>
2026-08-30 18:30:25 +0530

    Add Category model and EF Core database setup

---
+++ PageTurnerWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PageTurnerWeb.Models;
+
+namespace PageTurnerWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+     
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action" },
+                new Category { Id = 2, Name = "Science Fiction" },
+                new Category { Id = 3, Name = "History" }
+               
+            );
+        }
+
+    }
+}
---
+++ PageTurnerWeb/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PageTurnerWeb.Models
+{
+    public class Category
+    {
+        //[Key]- If PK name is not Id, use this attribute.
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+    }
+}
